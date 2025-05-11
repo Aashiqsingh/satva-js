8e5866ccf80e9989adcdd8880b2ef7b4f5fd5de9
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-11 14:07:11 +0530

    json array

---
+++ jsonArray/array1.js
@@ -0,0 +1,45 @@
+var students = [
+    {
+        id : 1,
+        name:"pranshul",
+        age:15,
+        email:"pranshul@gmail.com",
+        mobile:7826397878
+    },
+    {
+        id:2,
+        name:"rahul",
+        age:29,
+        email:"rahul@gmail.com",
+        mobile:87263978
+    },
+    {
+        id:3,
+        name:"rohan",
+        age:27,
+        email:"rohan@gmail.com",
+        mobile:9826397878
+    },
+    {
+        id:4,
+        name:"ronali",
+        age:24,
+        email:"sonali@gmail.com",
+        mobile:8726978
+    },
+    {
+        id:5,
+        name:"satva",
+        age:16,
+        email:"satva@gmail.com",
+        mobile:7669738338
+    }
+]
+
+
+// console.log(students);
+
+// students.forEach((stu)=>{
+//     console.log(stu.name);
+    
+// })
